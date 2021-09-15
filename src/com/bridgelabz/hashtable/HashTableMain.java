4910537431be0cfbde270d03ba7a1b4175b9eb16
Prashanth <imprash4u@gmail.com>
2021-09-15 20:45:34 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/hashtable/HashTableMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hashtable;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to HashTable Program");
+    }
+}
